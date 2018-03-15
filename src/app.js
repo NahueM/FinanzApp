5bcef087bcab5a4013c04bc8ce51e5c1efc47d48
NahueM <lnahuelmaya@gmail.com>
2018-03-15 16:21:23 -0300

    Remove test expense data

--- src/app.js
+++ src/app.js
@@ -12,16 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-
-store.dispatch(addExpense({ description: 'water bill', amount: 25000 }));
-store.dispatch(addExpense({ description: 'gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'rent', amount: 109500 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
