a430aebc651b5b7938a3c65ff2a2b2659ea54a00
NahueM <lnahuelmaya@gmail.com>
2018-03-16 11:33:44 -0300

    Setup formatting for amount and createdAt

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => (
     <header>
         <h1> FinanzApp </h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Agregar Gasto</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Pagina de Ayuda</NavLink> 
+        <NavLink to="/create" activeClassName="is-active"> Agregar Gasto</NavLink>
+        <NavLink to="/help" activeClassName="is-active"> Pagina de Ayuda</NavLink> 
     </header>
 );
 
